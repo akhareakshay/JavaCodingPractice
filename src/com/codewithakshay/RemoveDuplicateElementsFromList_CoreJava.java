3db6a9876fc855c177258d36cbe77cf09ddbd98a
akhareakshay <akhareakshay2@gmail.com>
2025-05-04 17:32:47 +0530

    remove duplicate elements - core java

---
+++ src/com/codewithakshay/RemoveDuplicateElementsFromList_CoreJava.java
@@ -0,0 +1,27 @@
+package com.codewithakshay;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+ * 2) How do you remove duplicate elements from a list using Java 8 streams?
+*/
+
+public class RemoveDuplicateElementsFromList_CoreJava {
+
+	public static void main(String[] args) {
+		
+		List<String> listOfStrings = Arrays.asList("Java", "Python", "C#", "Java", "Kotlin", "Python");
+
+		Set<String> uniqueStrings = new HashSet<>();
+		
+		for(int i = 0; i < listOfStrings.size(); i++) {
+			uniqueStrings.add(listOfStrings.get(i));
+		}
+		
+		System.out.println(uniqueStrings);
+	}
+
+}
